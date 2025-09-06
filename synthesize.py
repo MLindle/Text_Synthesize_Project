510a2c22ca8fb757b0647f8300590384c76a0ce4
Myzerberries <52772439+Myzerberries@users.noreply.github.com>
2025-09-06 10:32:02 -0500

    Adding beta cloudformation template, functionalized python script for lambda, workflow updates, and more.

--- synthesize.py
+++ synthesize.py
@@ -1,16 +1,27 @@
 import boto3
+import os
+from contextlib import closing
 
+bucket = os.environ["S3_Bucket"]
+prefix = os.environ.get("S3_Bucket_Prefix", "")
+
+s3 = boto3.client('s3')
 polly = boto3.client('polly')
 
-with open('speech.txt', 'r') as f:
-    text_to_synthesize = f.read()
+def synthesize():
 
-response = polly.synthesize_speech(
-    LanguageCode='en-US',
-    OutputFormat='mp3',
-    Text=text_to_synthesize,
-    VoiceId='Matthew'
-)
+    with open('speech.txt', 'r') as f:
+        text_to_synthesize = f.read()
 
-with open ("polly_output.mp3", "wb") as f:
-    f.write(response["AudioStream"].read())
+    response = polly.synthesize_speech(
+        LanguageCode='en-US',
+        OutputFormat='mp3',
+        Text=text_to_synthesize,
+        VoiceId='Matthew'
+    )
+
+    with closing(response["AudioStream"]) as stream:
+        audio_bytes = stream.read()
+
+    s3.put_object(Bucket=bucket, Key=prefix, Body=audio_bytes)
+    return {'bucket':bucket, 'key':prefix}
